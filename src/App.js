1d8b1be59126fba9f6744b03eee704ecf4c26825
Tenzin <tenthinten@gmail.com>
2022-01-31 21:13:05 +0530

    display original image file

--- src/App.js
+++ src/App.js
@@ -8,17 +8,24 @@ function App() {
   const[compressedImage,setCompressedImage] = useState('');
   const[fileName,setFileName] = useState('')
 
-  const handle = () => {
-    
+  const handle = (e) => {
+    const imageFile = e.target.files[0];
+    setOrigImage(imageFile);
+    setOrigImageFile(URL.createObjectURL(imageFile));
+    setFileName(imageFile.name);
   }
+
   return (
     <div className="App">
+      <h1></h1>
       <Container>
         <Grid>
           <Grid.Column width={6}>
             <Item>
-              <Image src = "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGUlMjBsYXB0b3B8ZW58MHx8MHx8&w=1000&q=80">
-              </Image>
+              {origImageFile ? (
+                <Image src={origImageFile}></Image>
+              ) :(<Image src = "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGUlMjBsYXB0b3B8ZW58MHx8MHx8&w=1000&q=80"></Image>
+              )}
             </Item>
           </Grid.Column>
           <Grid.Column width={4}>
@@ -26,14 +33,17 @@ function App() {
             accept="image/"
             className="mt-2 btn btn-dark w-75"
             onChange={(e) => handle(e)} />
+            <h1></h1>
             <Button>Compress Image</Button>
+            <h1></h1>
             <Button>Download Image</Button>
           </Grid.Column>
           <Grid.Column width={6}>
             <Item>
-            <Image src = "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGUlMjBsYXB0b3B8ZW58MHx8MHx8&w=1000&q=80">
-              </Image>
-            </Item>
+            {compressedImage ? (
+                <Image src={compressedImage}></Image>
+              ) :(<Image src = "https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YXBwbGUlMjBsYXB0b3B8ZW58MHx8MHx8&w=1000&q=80"></Image>
+              )}            </Item>
           </Grid.Column>
 
         </Grid>
